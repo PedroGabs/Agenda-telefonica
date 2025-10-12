3762cf48bb2b1436e5a4ab94ddd04c7d06fc84b5
DarK69f <Pedrocastrotuti@outlook.com>
2025-10-11 21:23:04 -0300

    v1.0.0 → versão final

--- projetinho.py
+++ projetinho.py
@@ -1,45 +1,80 @@
+import os
+import time
+
 agenda = []
 
 def adicionar_contato(nome,numero):
-        pessoa = {"nome" : nome, "numero" : numero}
-        agenda.append(pessoa)
+    pessoa = {"nome" : nome, "numero" : numero}
+    agenda.append(pessoa)
 
+def cadastroContato():
+    nome = input('\nDigite o nome do contato: ')
+    if len(nome) >= 50:
+        print("Nome muito grande! (limite de 50 caracteres)")
+    else:
+        while True:
+            try:
+                numero = input('Digite o número do contato com o DDD: ')
+                if numero.isdigit() and len(numero) == 11:
+                    print('Número cadastrado!\n')
+                    numero = int(numero)
+                    adicionar_contato(nome, numero)
+                    time.sleep(3)
+                    os.system('cls')
+                else:
+                    print("\nO número deve ter apenas 11 dígitos!\n")
+                break
+            except ValueError:
+                print('⚠ Digite apenas números!\n')
+
+def visualizarContatos():
+    if not agenda:
+        print("╔════════════════════════════════════════════════════╗ ")
+        print("║ Nenhum contato salvo.                              ║ ")
+        print("╚════════════════════════════════════════════════════╝")
+        time.sleep(3)
+        os.system('cls')
+    else:
+        print("\n📒 Contatos salvos:")
+    for contato in agenda:
+        print('╔════════════════════════════════════════════════════╗')
+        print(f"Nome: {contato['nome']} | Número: {contato['numero']}")
+        print('╚════════════════════════════════════════════════════╝\n')
+        time.sleep(18)
+        os.system('cls')
+
+#######################################     MENU     ########################################
 
 while True:
-    print('--------------------------------------------------------')
-    print('                   AGENDA TELEFÔNICA                    ')
-    print('--------------------------------------------------------')
-    print('[1] - Ver contatos')
-    print('[2] - Adicionar contatos')
-    print('[3] - Sair')
-    print()
-
-    resposta = input('R: ')
-
-    print()
+    print('╔══════════════════════════════════════════════════════╦════════════════════════════════════════╗')
+    print('║                  AGENDA TELEFÔNICA                   ║                                        ║')
+    print('╠══════════════════════════════════════════════════════╬════════════════════════════════════════╣')
+    print(r'║                                                      ║                                        ║')
+    print(r'║[1] - Ver contatos                                    ║             ______________             ║')
+    print(r'║[2] - Adicionar contatos                              ║            /   ,,____,,   \:.          ║')
+    print(r'║[3] - Sair                                            ║            |__| [][][] |__|:  :        ║')
+    print(r'║                                                      ║              /  [][][]  \   :  :       ║')
+    print(r'║                                                      ║             /   [][][]   \   :  :      ║')
+    print(r'║                                                      ║            /    [][][]    \   ..       ║')
+    print(r'║                                                      ║           |________________|           ║')
+    print(r'║                                                      ║                                        ║')
+    print('╚══════════════════════════════════════════════════════╩════════════════════════════════════════╝')
+    
+    try: 
+        resposta = int(input('R: '))
+    except ValueError:
+        print('\nUtilize apenas números!\n')
+        continue
 
     match resposta:
-        case '1':
-            if not agenda:
-                print("📭 Nenhum contato salvo.")
-            else:
-                print("📒 Contatos salvos:")
-            for contato in agenda:
-                print('--------------------------------------------------------')
-                print(f"Nome: {contato['nome']} | Número: {contato['numero']}")
-                print('--------------------------------------------------------')
+        case 1:
+            visualizarContatos()
 
-        case '2':
-            nome = input('Digite o nome do contato: ')
-            while True:
-                try:
-                    numero = int(input('Digite o número do contato: '))
-                    break
-                except ValueError:
-                    print('⚠ Digite apenas números!')
+        case 2:
+            cadastroContato()
 
-            adicionar_contato(nome, numero)
-
-        case '3':
+        case 3:
             break
 
+        case _:
+            print("\nOpção inválida!\n")
